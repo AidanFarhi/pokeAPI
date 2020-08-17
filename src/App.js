5cc34fd1a5d96fee9f2edbe448cbe77178994fba
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-16 22:43:19 -0400

    got list of pokes to show up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 export default function App() {
   const[state, setState] = useState({
-    pokemon: {}
+    names: [],
+    isLoading: true
+  })
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=150`)
+      const json = await response.json()
+      const res = []
+      for (let poke of json.results) {
+        res.push(<li>{poke.name}</li>)
+      }
+      setState({names: res, isLoading: false})
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(()=> {
+    if (state.isLoading) {
+      fetchData()
+    }
   })
 
   return (
     <div>
-      pokemon
+      <ul>
+        {state.names}
+      </ul>
     </div>
   )
 }
